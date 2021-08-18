b5aebdb09aab0a2ee6143110fb7eb916b823890c
rajpalvaghela <rajpal.vag@gmail.com>
2021-08-18 12:13:27 +1000

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class Application {
  
     public static void main(String[] args) {
-        System.out.println("Hello World!This is a new commit 1");
+        System.out.println("Hello World!This is a new commit 2");
     }
  
 }
